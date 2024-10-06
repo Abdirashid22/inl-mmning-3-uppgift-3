6a49e41f171ed5dcefe63d67710b7a20129dc986
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-10-06 20:02:05 +0200

    Add project files.

---
+++ inlämmning 3 uppgift 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många dagar vill du hyra bilen");
+            int antaldagar=int.Parse(Console.ReadLine());
+            Console.Write("Hur många kilometer ska du köra? ");
+            int antalKilometer = int.Parse(Console.ReadLine());
+
+            int totalKostnad = 300 + ((antaldagar - 1) * 500) + (antalKilometer * 1);
+
+            Console.WriteLine("Den totala kostnaden för hyran är " + totalKostnad + " kr");
+
+        }
+    }
+}
